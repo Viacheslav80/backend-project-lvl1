f93c55dee5455dadeb4138e9a88e56c8a271bd8e
Slava Surikov <slava@surikov.io>
2019-07-24 00:22:31 +0300

    game1_v1.7.10

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/bf47194235807c007714/maintainability)](https://codeclimate.com/github/Viacheslav80/backend-project-lvl1/maintainability)
 [![Build Status](https://travis-ci.org/Viacheslav80/backend-project-lvl1.svg?branch=master)](https://travis-ci.org/Viacheslav80/backend-project-lvl1)
 
-Game-even
+# Brain-games clone project.
 
-[![asciicast](https://asciinema.org/a/vBTFWbB9K2JPIDb1Tmmd6SyRC.svg)](https://asciinema.org/a/vBTFWbB9K2JPIDb1Tmmd6SyRC)
+## installing
+ 
+build the pocket.
+
+```
+$ uses_folder/make publish 
+```
+install pocket of global
+
+```
+$ uses_folder/sudo npm link
+```
+
+use:
+
+```
+$ brain-even
+$ brain-calc
+$ brain-gcd
+$ brain-prime
+$ brain-prograssion
+```
+
+Game-even
+[![asciicast](https://asciinema.org/a/CcsHCEEz5XcF2xQtmRXP09Gj2.svg)](https://asciinema.org/a/CcsHCEEz5XcF2xQtmRXP09Gj2)
 
 Game-calc
-[![asciicast](https://asciinema.org/a/OIE3Cg1fgLzciJ1RuY2UDntGA.svg)](https://asciinema.org/a/OIE3Cg1fgLzciJ1RuY2UDntGA)
+[![asciicast](https://asciinema.org/a/ebJYR66hU6MQk8PXKR6t59bnn.svg)](https://asciinema.org/a/ebJYR66hU6MQk8PXKR6t59bnn)
 
 Game-gcd
 [![asciicast](https://asciinema.org/a/BvvkOI8CBH7whxXThbeFRb8KS.svg)](https://asciinema.org/a/BvvkOI8CBH7whxXThbeFRb8KS)
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,26 +1,19 @@
-import {
-  cons, car, cdr,
-} from '@hexlet/pairs';
+import { cons } from '@hexlet/pairs';
 import { executionGame, getRandomInRange } from '..';
 
 const task = 'What is the result of the expression?';
+const add = (num1, num2) => [num1 + num2, '+'];
+const sub = (num1, num2) => [num1 - num2, '-'];
+const mul = (num1, num2) => [num1 * num2, '*'];
 const makeQuestionAndAnswer = () => {
   const num1 = getRandomInRange(1, 15);
   const num2 = getRandomInRange(1, 15);
-  const numOperation = getRandomInRange(0, 2);
-  const expressions = cons((num1 - num2), cons((num1 + num2), (num2 * num2)));
-  let question;
-  let trueAnswer;
-  switch (numOperation) {
-    case 0: trueAnswer = String(car(cdr(expressions)));
-      question = `${num1} + ${num2}`; break;
-    case 1: trueAnswer = String(car(expressions));
-      question = `${num1} - ${num2}`; break;
-    case 2: trueAnswer = String(cdr(cdr(expressions)));
-      question = `${num1} * ${num2}`; break;
-    default: break;
-  }
-
+  const numFirstOperation = 0;
+  const numEndOperation = 2;
+  const randomOperationByNum = getRandomInRange(numFirstOperation, numEndOperation);
+  const operations = [add(num1, num2), sub(num1, num2), mul(num1, num2)];
+  const trueAnswer = `${operations[randomOperationByNum][0]}`;
+  const question = `${num1} ${operations[randomOperationByNum][1]} ${num2}`;
   return cons(question, trueAnswer);
 };
 export default () => {
