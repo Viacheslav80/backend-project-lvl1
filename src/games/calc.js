143fad73dd83c9e4e0f05de44db3e245ef1a9b26
Slava Surikov <slava@surikov.io>
2019-07-25 00:21:38 +0300

    game1_v1.7.13

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,18 +2,24 @@ import { cons } from '@hexlet/pairs';
 import { executionGame, getRandomInRange } from '..';
 
 const task = 'What is the result of the expression?';
-const add = (num1, num2) => [num1 + num2, '+'];
-const sub = (num1, num2) => [num1 - num2, '-'];
-const mul = (num1, num2) => [num1 * num2, '*'];
 const makeQuestionAndAnswer = () => {
   const num1 = getRandomInRange(1, 15);
   const num2 = getRandomInRange(1, 15);
   const numFirstOperation = 0;
   const numEndOperation = 2;
+  let trueAnswer;
+  let question;
   const randomOperationByNum = getRandomInRange(numFirstOperation, numEndOperation);
-  const operations = [add(num1, num2), sub(num1, num2), mul(num1, num2)];
-  const trueAnswer = `${operations[randomOperationByNum][0]}`;
-  const question = `${num1} ${operations[randomOperationByNum][1]} ${num2}`;
+  const operations = ['add', 'sub', 'mul'];
+  switch (randomOperationByNum) {
+    case operations.indexOf('add'): trueAnswer = `${num1 + num2}`;
+      question = `${num1} + ${num2}`; break;
+    case operations.indexOf('sub'): trueAnswer = `${num1 - num2}`;
+      question = `${num1} - ${num2}`; break;
+    case operations.indexOf('mul'): trueAnswer = `${num1 * num2}`;
+      question = `${num1} * ${num2}`; break;
+    default: break;
+  }
   return cons(question, trueAnswer);
 };
 export default () => {
