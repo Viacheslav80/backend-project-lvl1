ab616d5fd25e7c37e1ed530be7c25873175a3833
Slava Surikov <slava@surikov.io>
2019-07-27 18:24:43 +0300

    game1_v1.7.15

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,11 @@
 import { cons } from '@hexlet/pairs';
 import { playGame, getRandomInRange } from '..';
 
-const isEven = num => ((num % 2) ? 'no' : 'yes');
+const isEven = num => (num % 2);
 const task = 'Answer "yes" if number even otherwise answer "no"';
 const makeQuestionAndAnswer = () => {
   const question = getRandomInRange(1, 15);
-  const trueAnswer = isEven(question);
+  const trueAnswer = isEven(question) ? 'no' : 'yes';
   return cons(question, trueAnswer);
 };
 export default () => {
--- src/games/prime.js
+++ src/games/prime.js
@@ -3,15 +3,14 @@ import { playGame, getRandomInRange } from '..';
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 const isPrime = (number) => {
-  for (let i = 2; i < number; i += 1) {
-    if (number % i === 0) return 'no';
+  for (let i = 2; i <= number / 2; i += 1) {
+    return (number % i === 0);
   }
-  return (number === 1) ? 'no' : 'yes';
+  return (number === 1);
 };
 const makeQuestionAndAnswer = () => {
-  const number = getRandomInRange(1, 10);
-  const trueAnswer = isPrime(number);
-  const question = `${number}`;
+  const question = getRandomInRange(1, 10);
+  const trueAnswer = isPrime(question) ? 'no' : 'yes';
   return cons(question, trueAnswer);
 };
 export default () => {
